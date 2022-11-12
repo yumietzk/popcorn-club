3e0e4cf5345a53795c1e0bbe8b80816533d39a56
yumietzk <yumie.t.29@gmail.com>
2022-11-12 12:04:50 -0500

    update a json server url

--- src/apis/favorites.js
+++ src/apis/favorites.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: 'http://localhost:3001',
-  baseURL: 'https://json-server-myproject.herokuapp.com',
+  baseURL: 'https://json-server-g3wl.onrender.com',
 });
